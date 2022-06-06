28445b556bc42f75982844c28b1896284d7c6d42
lopdiu2001 <106945302+lopdiu2001@users.noreply.github.com>
2022-06-06 12:59:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     btnNo: 'Nhìu siu to khổng lồ',
     question: 'Cảm nghĩ của bạn về sản phẩm xịn xò này sau 4 tháng?',
     btnReply: 'Gửi cho tui <3',
-    reply:,
+
     mess: 'okela, lớp diu xâu mớt',
     messDesc: 'Chúc chúng mình hạnh phúc vui vẻ mạnh khỏe :))))',
     btnAccept: 'Okiiiii lun <3',
