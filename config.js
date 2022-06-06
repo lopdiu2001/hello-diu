ba9b0577d75927a990be4ee05b6d6ced98ab7ae1
lopdiu2001 <106945302+lopdiu2001@users.noreply.github.com>
2022-06-06 12:50:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "",
+    introTitle: 'Lớp diu',
+    introDesc: `Hế lu bạn ni`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    title: 'Bạn ni yêu bạn nhiều từng mô?',
+    desc: 'Ê làm cho vui chớ khum biết chi hết :))))',
+    btnYes: 'Khum nhìu lắm',
+    btnNo: 'Nhìu siu to khổng lồ',
+    question: 'Cảm nghĩ của bạn về sản phẩm xịn xò này sau 4 tháng?',
+    btnReply: 'Gửi cho tui <3',
+    reply: 'Tùy diu viết',
+    mess: 'okela, lớp diu xâu mớt',
+    messDesc: 'Chúc chúng mình hạnh phúc vui vẻ mạnh khỏe :))))',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/messages/t/100005637903599' 
 }
